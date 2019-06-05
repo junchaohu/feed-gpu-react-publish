964635b59f4287c63ea972a402ffb8b31909ebd7
junchao hu <hjc_1208@126.com>
2019-06-05 10:31:59 +0800

    feed gpu react published

---
+++ api.js
@@ -0,0 +1,4 @@
+function getAPI(){
+    return "http://10.111.212.133:8886/";
+    //return "http://"+window.location.hostname+":8080/";
+}
---
+++ precache-manifest.da001a927775efdc003ae330ac4ed2d3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "2566cc6cac6f47fa69e5",
+    "url": "/static/js/7.068030bf.chunk.js"
+  },
+  {
+    "revision": "da513db52789d2c91266",
+    "url": "/static/css/main.861c0871.chunk.css"
+  },
+  {
+    "revision": "074cc3979b6dfb0aae6b",
+    "url": "/static/js/runtime~main.936d573f.js"
+  },
+  {
+    "revision": "6c60c3d52b66a0d0c45be694284cbf55",
+    "url": "/static/media/LOGO_dashboard.6c60c3d5.png"
+  },
+  {
+    "revision": "8d7f98dbb982910bcdd2",
+    "url": "/static/js/2.1fa0cd85.chunk.js"
+  },
+  {
+    "revision": "ab275d232a83a04b3e29",
+    "url": "/static/js/3.4a8cad38.chunk.js"
+  },
+  {
+    "revision": "a471ddc06021cb84b610",
+    "url": "/static/js/4.9aba416c.chunk.js"
+  },
+  {
+    "revision": "24eb672d37fba49c15a3",
+    "url": "/static/js/5.143c24ba.chunk.js"
+  },
+  {
+    "revision": "62ac5e5f154446af028c",
+    "url": "/static/js/6.b9b445e5.chunk.js"
+  },
+  {
+    "revision": "da513db52789d2c91266",
+    "url": "/static/js/main.7f73d2ae.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "c4c441039c3c929e8e3cc45ac86f1933",
+    "url": "/static/media/dashboard-bg.c4c44103.jpg"
+  },
+  {
+    "revision": "8d7f98dbb982910bcdd2",
+    "url": "/static/css/2.609fead2.chunk.css"
+  },
+  {
+    "revision": "16d63348c3b36a4beb15e4cdce75e3d1",
+    "url": "/index.html"
+  }
+];
